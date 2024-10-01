7e5b4010da98c38a21b3a20ec00fed87a496302f
AhmedEhabH <ahmed.ehab.h@gmail.com>
2024-10-01 15:34:45 +0300

    using routing

--- AuthECClient/src/app/app.routes.ts
+++ AuthECClient/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+import {LoginComponent} from './user/login/login.component';
+import {
+    RegistrationComponent
+} from './user/registration/registration.component';
+import {UserComponent} from './user/user.component';
+
+export const routes: Routes = [ {
+    path : 'user',
+    component : UserComponent,
+    children : [
+        {path : 'signup', component : RegistrationComponent},
+        {path : 'login', component : LoginComponent}
+    ]
+} ];
